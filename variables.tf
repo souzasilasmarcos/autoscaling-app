9c9091cabbdcfe396939db54db1f15bfe77a1e18
Silas Marcos <30444491+souzasilasmarcos@users.noreply.github.com>
2021-09-20 23:53:23 -0300

    Ajuste 1.0.9
     Ajuste 1.0.9 de projeto em homologacao autoscaling-app

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+  type        = string
+  description = ""
+  default     = "sa-east-1"
+}
+
+variable "aws_profile" {
+  type        = string
+  description = ""
+  default     = "dev-key-admin-sp"
+}
+
+variable "aws_account_id" {
+  type        = number
+  description = ""
+  default     = 374120343751
+}
+
+variable "service_name" {
+  type        = string
+  description = ""
+  default     = "autoscaling-app"
+}
+
+variable "instance_type" {
+  type        = string
+  description = ""
+  default     = "t3.micro"
+}
+
+variable "instance_key_name" {
+  type        = string
+  description = ""
+  default     = "devops.noob"
+}
---
+++ vpc.tf
@@ -0,0 +1,50 @@
+resource "aws_vpc" "this" {
+  cidr_block = "172.31.0.0/16"
+  tags       = merge(local.common_tags, { Name = "Terraform VPC " })
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+  tags   = merge(local.common_tags, { Name = "Terraform IGW " })
+}
+
+resource "aws_subnet" "this" {
+  for_each = {
+    "pub_a" : ["172.31.0.0/20", "${var.aws_region}a", "Public A"]
+    "pub_b" : ["172.31.32.0/20", "${var.aws_region}b", "Public B"]
+	"pvt_c" : ["172.31.16.0/20", "${var.aws_region}c", "Private C"]
+    "pvt_a" : ["172.31.0.0/20", "${var.aws_region}a", "Private A"]
+    "pvt_b" : ["172.31.32.0/20", "${var.aws_region}b", "Private B"]
+	"pvt_c" : ["172.31.16.0/20", "${var.aws_region}c", "Private C"]
+	
+  }
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = each.value[0]
+  availability_zone = each.value[1]
+
+  tags = merge(local.common_tags, { Name = each.value[2] })
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = merge(local.common_tags, { Name = "Terraform Public" })
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+  tags   = merge(local.common_tags, { Name = "Terraform Private" })
+}
+
+resource "aws_route_table_association" "this" {
+  for_each = local.subnet_ids
+
+  subnet_id      = each.value
+  route_table_id = substr(each.key, 0, 3) == "Pub" ? aws_route_table.public.id : aws_route_table.private.id
+}
