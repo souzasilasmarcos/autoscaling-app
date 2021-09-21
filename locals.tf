3bbe1d19da897748b6145de30592647983dfb7a4
souzasilasmarcos <souzasilasmarcos@gmail.com>
2021-09-21 00:14:42 -0300

    correcao subnet region autoscaling-app

--- locals.tf
+++ locals.tf
@@ -2,10 +2,10 @@ locals {
   subnet_ids = { for k, v in aws_subnet.this : v.tags.Name => v.id }
 
   common_tags = {
-    Project   = "Curso AWS com Terraform"
+    Project   = "Iac com Terraform"
     CreatedAt = "2020-03-17"
     ManagedBy = "Terraform"
-    Owner     = "Cleber Gasparoto"
+    Owner     = "Silas Marcos"
     Service   = "Auto Scaling App"
   }
 }
