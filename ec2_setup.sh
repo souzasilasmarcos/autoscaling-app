5499dab4a5135dd94c13237c4718e1828fe3a1ba
souzasilasmarcos <souzasilasmarcos@gmail.com>
2021-09-27 21:18:56 -0300

    Ajuste no script do userdata

--- ec2_setup.sh
+++ ec2_setup.sh
@@ -15,8 +15,3 @@ sudo apt  install docker.io
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_NUMBER.dkr.ecr.$REGION.amazonaws.com
 
 docker push $ACCOUNT_NUMBER.dkr.ecr.$REGION.amazonaws.com/sample-nodejs-app:latest
-=======
-
-# sudo stress-ng --cpu 32 --timeout 180 --metrics-brief
-
-# subida de aplicacao em aplicacao web acesso via ALB
