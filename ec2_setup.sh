8bd55bdbb53c759bfb9e76576f3f8b7aaa04477b
souzasilasmarcos <souzasilasmarcos@gmail.com>
2021-09-28 00:01:44 -0300

    Ajuste do no script userdata para push docker

--- ec2_setup.sh
+++ ec2_setup.sh
@@ -1,17 +1,21 @@
-#!/bin/bash
+q#!/bin/bash
+
+REGION="sa-east-1"
+ACCOUNT_NUMBER=374120343751
+
 sudo apt-get update
-#sudo apt-get install -y apache2
-#echo 'Piloto biruta' > /var/www/html/index.html
-#service httpd start
 
-sudo apt-get install -y stress-ng
-
-sudo apt  install docker.io
+sudo apt install docker.io -y
 
 # sudo stress-ng --cpu 32 --timeout 180 --metrics-brief
-
 # subida de aplicacao em aplicacao web acesso via ALB
 
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_NUMBER.dkr.ecr.$REGION.amazonaws.com
 
+docker tag sample-nodejs-app:latest $ACCOUNT_NUMBER.dkr.ecr.$REGION.amazonaws.com/sample-nodejs-app:latest
+
+docker pull $ACCOUNT_NUMBER.dkr.ecr.$REGION.amazonaws.com/sample-nodejs-app:latest
+
 docker push $ACCOUNT_NUMBER.dkr.ecr.$REGION.amazonaws.com/sample-nodejs-app:latest
+
+docker run -p 80:3000 $ACCOUNT_NUMBER.dkr.ecr.$REGION.amazonaws.com/sample-nodejs-app
