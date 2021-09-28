b866a2ca451b0172abc3c3fa5e7a09b79a086199
souzasilasmarcos <souzasilasmarcos@gmail.com>
2021-09-27 21:16:19 -0300

    Ajuste no script do userdata

--- ec2_setup.sh
+++ ec2_setup.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y apache2
-echo 'Piloto biruta' > /var/www/html/index.html
-service httpd start
+#sudo apt-get install -y apache2
+#echo 'Piloto biruta' > /var/www/html/index.html
+#service httpd start
 
 sudo apt-get install -y stress-ng
 
 sudo apt  install docker.io
+
+# sudo stress-ng --cpu 32 --timeout 180 --metrics-brief
+
+# subida de aplicacao em aplicacao web acesso via ALB
+
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_NUMBER.dkr.ecr.$REGION.amazonaws.com
+
+docker push $ACCOUNT_NUMBER.dkr.ecr.$REGION.amazonaws.com/sample-nodejs-app:latest
+=======
 
 # sudo stress-ng --cpu 32 --timeout 180 --metrics-brief
 
